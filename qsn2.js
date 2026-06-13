a0c62f7124c656edf9e4b5fb2895fc1268d42f3f
Praptee <taptitahsin5@gmail.com>
2026-06-13 22:04:09 +0600

    scnd qsn done

---
+++ qsn2.js
@@ -0,0 +1,8 @@
+const numbers = [10, 20, 30, 40, 50];
+
+const doubled = numbers.map((num) => num * 2);
+console.log("Doubled:", doubled);
+
+
+const filtered = numbers.filter((num) => num > 25);
+console.log("Greater than 25:", filtered);
